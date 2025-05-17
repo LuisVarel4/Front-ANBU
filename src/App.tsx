888054795b9fdf0e67dba5624472a411f769ec64
LuisVarel4 <luvarelao@unal.edu.co>
2025-05-16 22:19:58 -0500

    starting

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-red-anbu'>HOla mundo</h1>
+      <h1 className='bg-green-anbu'>HOla mundo</h1>
+      <h1 className='bg-grayBlue-anbu'>HOla mundo</h1>
+      <h1 className='bg-yellow-anbu'>HOla mundo</h1>
+      <h1 className='bg-gray1-anbu'>HOla mundo</h1>
+      <h1 className='bg-gray2-anbu'>HOla mundo</h1>
+      <h1 className='bg-gray3-anbu'>HOla mundo</h1>
+      <h1 className='bg-black-anbu'>HOla mundo</h1>
+    
+      
+    </>
+  )
+}
+
+export default App
