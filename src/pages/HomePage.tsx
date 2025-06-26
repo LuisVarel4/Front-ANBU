fb9cb0fb7301df7b858921107f64e5814e89a80c
Straver00 <macuino.555@gmail.com>
2025-06-26 03:25:57 -0500

    add background.webp

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import Welcome from "../features/Welcome";
 import Traitors from "../features/Traitors";
 import CustomBr from "../components/CustomBr";
-import background from "../assets/ilustrations/itachi_bg.svg";
+import background from "../assets/ilustrations/bg-dashboard.webp";
 import React from "react";
 
 const HomePage: React.FC = () => {
